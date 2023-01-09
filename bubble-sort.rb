cb2a139cbb7491ea208678a783a6a73148099a19
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-09 22:33:06 +1300

    Create bubble_sort pseudocode

---
+++ bubble-sort.rb
@@ -0,0 +1,10 @@
+# function bubble_sort takes one argument: numbers (array)
+    # create loop count var (number = 0)
+    # create swapped numbers var (bool = false)
+    # until swapped numbers is true
+        # loop through entire array - 1 - loop count
+            # check if current element > next element
+                # swap elements
+                # set swapped numbers to true
+        # increment loop count
+    # return numbers
