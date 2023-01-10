24eb1698ea37cb9da85829b4e55d224147a3021c
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-10 14:25:04 +1300

    Update pseudocode

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,10 +1,19 @@
 # function bubble_sort takes one argument: numbers (array)
     # create loop count var (number = 0)
+    # create stop until loop var (bool = false)
     # create swapped numbers var (bool = false)
-    # until swapped numbers is true
+    # loop until 'stop until loop' is true
         # loop through entire array - 1 - loop count
             # check if current element > next element
                 # swap elements
                 # set swapped numbers to true
-        # increment loop count
+        # if swapped numbers is true
+            # increment loop count
+            # set swapped numbers to false
+        # else
+            # set 'stop until loop' to true
     # return numbers
+
+def bubble_sort(numbers)
+
+end
