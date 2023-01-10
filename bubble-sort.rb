25291aded062896c96ac54e5549f31c2b3f5c25b
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-10 14:38:58 +1300

    Complete bubble_sort

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -3,7 +3,7 @@
     # create stop until loop var (bool = false)
     # create swapped numbers var (bool = false)
     # loop until 'stop until loop' is true
-        # loop through entire array - 1 - loop count
+        # loop through entire array - 2 - loop count
             # check if current element > next element
                 # swap elements
                 # set swapped numbers to true
@@ -15,5 +15,28 @@
     # return numbers
 
 def bubble_sort(numbers)
+    loop_count = 0
+    stop_until_loop = false
 
+    until stop_until_loop
+        swapped_numbers = false
+        for i in 0..numbers.length - 2 - loop_count do
+            if numbers[i] > numbers[i + 1]
+                large_num_temp = numbers[i]
+                numbers[i] = numbers[i + 1]
+                numbers[i + 1] = large_num_temp
+                swapped_numbers = true
+            end
+        end
+        if swapped_numbers
+            loop_count += 1
+        else
+            stop_until_loop = true
+        end
+    end
+
+    numbers
 end
+
+sorted_numbers = bubble_sort([4,3,78,2,0,2])
+p sorted_numbers
